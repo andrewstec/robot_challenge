6f9632d1d91e50f900c7b091df98a98e381d720b
Andrew Stec <andrewstec@gmail.com>
2021-01-19 18:56:11 -0800

    Fixing endpoint name; loading queryParser to parse direction params

--- server.ts
+++ server.ts
@@ -1,11 +1,14 @@
 import * as restify from 'restify';
-
 const app: restify.Server = restify.createServer({name: 'Robot Servo'});
 
 const respond = (req: restify.Request, res: restify.Response, next: restify.Next) => { res.send('hello ' + req.params.name); next(); };
 
 const server = restify.createServer();
-server.get('/directions', respond);
+
+// Load plugins
+server.use(restify.plugins.queryParser());
+
+server.get('/direction', respond);
 
 server.listen(8080, () => {
     console.log('%s listening at %s', server.name, server.url);
