754998e7b5595d1e57282a451486b077ea69d0d7
Andrew Stec <andrewstec@gmail.com>
2021-01-20 19:49:19 -0800

    Adding params to make test pass

--- tests/direction.controller.test.ts
+++ tests/direction.controller.test.ts
@@ -3,7 +3,8 @@ import {server} from '../server';
 
 describe("GET /direction - What way should I go?", () => {
   it("should go left when shorter distance than going right", async () => {
-    const res = await request(server).get('/direction');
+    const res = await request(server).get('/direction?heading=310&target=75');
+    expect(res.status).toEqual(200);
   });
 });
 test('should go left when shorter distance than going right', () => {
