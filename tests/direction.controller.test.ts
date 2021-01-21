3866ce4981f90a62c1f94ce4f7743cbc272897f8
Andrew Stec <andrewstec@gmail.com>
2021-01-20 20:19:06 -0800

    Adding final tests to manage error scenarios

--- tests/direction.controller.test.ts
+++ tests/direction.controller.test.ts
@@ -19,4 +19,16 @@ describe("GET /direction - What way should I go?", () => {
     expect(res.status).toEqual(200);
     expect(res.body).toHaveProperty('direction', 'straight');
   });
+
+  it("should throw an invalid number error when a string is submitted instead of a number", async () => {
+    const res = await request(server).get('/direction?heading=310&target=helloWorld!');
+    expect(res.status).toEqual(500);
+    expect(res.body).toHaveProperty('error', 'Heading and target must be valid numbers');
+  });
+
+  it("should throw a missing param error when a param is missing from payload", async () => {
+    const res = await request(server).get('/direction?heading=310');
+    expect(res.status).toEqual(500);
+    expect(res.body).toHaveProperty('error', 'Heading and target are required');
+  });
 });
