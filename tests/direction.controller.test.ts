d96e9a3d6a3e1a0c226a31b5e7793d8608579418
Andrew Stec <andrewstec@gmail.com>
2021-01-20 20:08:09 -0800

    Adding test to capture when to go right

--- tests/direction.controller.test.ts
+++ tests/direction.controller.test.ts
@@ -5,12 +5,12 @@ describe("GET /direction - What way should I go?", () => {
   it("should go left when shorter distance than going right", async () => {
     const res = await request(server).get('/direction?heading=310&target=75');
     expect(res.status).toEqual(200);
+    expect(res.body).toHaveProperty('direction', 'right');
   });
-});
-test('should go left when shorter distance than going right', () => {
 
-});
-
-test('should go right when shorter distance than going left', () => {
-
+  it("should go right when shorter distance than going left", async () => {
+    const res = await request(server).get('/direction?heading=310&target=305');
+    expect(res.status).toEqual(200);
+    expect(res.body).toHaveProperty('direction', 'left');
+  });
 });
