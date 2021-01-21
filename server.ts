038c05de19ab5d555ba4d696363457c26bd80109
Andrew Stec <andrewstec@gmail.com>
2021-01-20 19:43:43 -0800

    Abstracted server/app listen function to avoid listening error in tests

--- server.ts
+++ server.ts
@@ -13,6 +13,4 @@ server.use(restify.plugins.queryParser({mapParams: true}));
 
 server.get('/direction', controller.get);
 
-server.listen(8080, () => {
-    console.log('%s listening at %s', server.name, server.url);
-});
+export {server};
